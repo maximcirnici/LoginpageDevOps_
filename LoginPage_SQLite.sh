bd27a3292f9e16e54f096d8741e4c23f75a2f1a0
maximcirnici <43680146+maximcirnici@users.noreply.github.com>
2021-01-11 18:33:05 +0000

    Update LoginPage_SQLite.sh

--- LoginPage_SQLite.sh
+++ LoginPage_SQLite.sh
@@ -11,7 +11,6 @@ cp -r static/* tempdir/static/.
 echo "FROM python" >> tempdir/Dockerfile
 echo "RUN pip install flask" >> tempdir/Dockerfile
 echo "RUN pip install pyotp" >> tempdir/Dockerfile
-echo "RUN sudo apt-get install curl" >> tempdir/Dockerfile
 echo "COPY  ./static /home/login/static/" >> tempdir/Dockerfile
 echo "COPY  ./templates /home/login/templates/" >> tempdir/Dockerfile
 echo "COPY  LoginPage_SQLite.py /home/login/" >> tempdir/Dockerfile
